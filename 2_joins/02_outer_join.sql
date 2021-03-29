53479a55a4dab4cabc2a2ea26edd6e1595051066
code-driving <tittan7@gmail.com>
2021-03-29 21:28:39 +0000

    BootcampX queries 2-JOIN

--- 2_joins/02_outer_join.sql
+++ 2_joins/02_outer_join.sql
@@ -5,10 +5,11 @@ ON cohorts.id = cohort_id;
 
 -- SELECT students.name as student_name, email, cohorts.name as cohort_name
 -- FROM students 
--- RIGHT OUTER JOIN cohorts 
+-- RIGHT JOIN cohorts 
 -- ON cohorts.id = cohort_id;
 
 -- SELECT students.name as student_name, email, cohorts.name as cohort_name
 -- FROM students 
--- FULL OUTER JOIN cohorts 
+-- FULL JOIN cohorts 
 -- ON cohorts.id = cohort_id;
+
