34e4cb654f197ed5d3f2928695ca2fbc9b9a286a
code-driving <tittan7@gmail.com>
2021-03-29 23:38:07 +0000

    Bootcampx queries 3-Group By

---
+++ 3_queries_group-by/03_large_cohorts.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name as cohort_name, count(cohort_id) AS student_count
+FROM students
+JOIN cohorts
+ON students.cohort_id = cohorts.id
+GROUP BY cohorts.name
+HAVING count(cohort_id) >= 18
+ORDER BY student_count;
