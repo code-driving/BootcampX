35caefe6dd724926455df9ea33c48717f2c2d197
code-driving <tittan7@gmail.com>
2021-04-01 01:29:27 +0000

    Use parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,10 +1,10 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'bootcampx'
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
 });
 
 pool.query(`
@@ -13,11 +13,15 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = '$1'
 ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
+`);
+
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values).then((res) => {
+  res.rows.forEach((row) => {
     console.log(`${row.cohort}: ${row.teacher}`);
-  })
+  });
 });
